5d15494c382f20112c227eb31616ca3411cf0ddb
Niyi Solomon Adebayo <adebayoniyi2000@yahoo.com>
2021-04-27 21:07:13 +0300

    end of lab 1

--- Lab1/task3.py
+++ Lab1/task3.py
@@ -3,18 +3,19 @@ import time
 
 sec = 0.003
 #leds = [29,31,33,35,37,40,38]
-leds = [29,31,33,35,37,36,38,40]
+leds = [29,31,33,35,37,36,38]
 
-num0 = [29,31,33,36,38,40],
-num1 = [29,36]
-num2 = [31,29,37,40,38]
-num3 = [31,29,37,38,36]
-num4 = [33,37,29,36]
-num5= [31,33,37,36,38]
-num6= [31,33,37,36,38,40]
-num7= [31,29,36]
-num8= [31,29,33,36,37,38,40]
-num9= [29,31,33,37,36]
+num0 = [29,31,33,37,38,36],
+num1 = [29,37]
+num2 = [31,29,35,38,36]
+num3 = [31,29,37,35,36]
+num4 = [33,37,29,35]
+num5 = [31,33,35,37,36]
+num6 = [31,33,37,36,38,35]
+num7 = [31,29,37]
+num8 = [31,29,33,36,37,38,35]
+num9 = [29,31,33,35,36,37]
+#dot  [40]
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -29,60 +30,44 @@ def pin_lighter(pin,pause):
     
 
 while True:
+    time.sleep(sec)
     for n in range(50):
         for i in num0:
             pin_lighter(i, sec)
-            
-     
-    #time.sleep(0.5)
-
-#     time.sleep(0.5)
+   
+    for n in range(50):
+        for i in num1:
+            pin_lighter(i, sec)
     
     for n in range(50):
         for i in num2:
             pin_lighter(i, sec)
-# #         
-#     time.sleep(0.5)
     
     for n in range(50):
         for i in num3:
             pin_lighter(i, sec)
-        
-    #time.sleep(0.5)
 
     for n in range(50):
         for i in num4:
             pin_lighter(i, sec)
-        
-    #time.sleep(0.5)
 
     for n in range(50):
         for i in num5:
             pin_lighter(i, sec)
         
-    #time.sleep(0.5)
-    
     for n in range(50):
         for i in num6:
             pin_lighter(i, sec)
         
-    #time.sleep(0.5)
-    
     for n in range(50):
         for i in num7:
             pin_lighter(i, sec)
-        
-    #time.sleep(0.5)
     
     for n in range(50):
         for i in num8:
             pin_lighter(i, sec)
-        
-    #time.sleep(0.5)
     
     for n in range(50):
         for i in num9:
             pin_lighter(i, sec)
-        
-    #time.sleep(0.5)
 GPIO.cleanup()    
