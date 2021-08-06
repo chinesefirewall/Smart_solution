183f6a244e90368b250bd0f2a8ff99e8b837caed
Niyi Solomon Adebayo <adebayoniyi2000@yahoo.com>
2021-08-06 06:59:16 +0300

    lab 03 complete

---
+++ Lab03/Lab03_task02_uart_connection.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import serial
+import time
+import sys
+# Other ports
+# /dev/ttyUSB0
+# /dev/ttyS0
+# /dev/ttyAMA0
+try:
+    #open port /dev/ttyUSB0 at 9600 bps
+    ser=serial.Serial('/dev/serial0', 9600, timeout=1)
+    ser.flush()
+    
+    #Let's print port name
+    print(ser.name)
+    
+    while True:
+        command = input("Please enter ON-OFF or sensor: ")
+        ser.write(command.encode())
+        
+        time.sleep(1)
+        print(ser.in_waiting)
+        #read the buffer with newlines and cut the newline and display
+        read_serial=ser.readline().decode('ascii').rstrip('\n')
+        print(read_serial)
+        
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    ser.close()
+    print("Existing program successfully...")
+    sys.exit()
